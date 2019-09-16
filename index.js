bcbb320d2acc997c50fe538c2d5cedec5013ed69
Rory Murray <rory.christian.murray@gmail.com>
2019-09-16 08:21:08 -0400

    Setup server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require("express");
+
+// import db file
+
+const db = require("./data/db"); // use db to get access to database
+
+// db has a find(), findById(), insert(), update(), and remove() methods
+// which we will use to access the database
+
+const server = express();
+
+// tell express to parse JSON
+server.use(express.json());
