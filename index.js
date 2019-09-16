fe7d30adf1fd5307e6ae85b90de180dd71b06f9b
Rory Murray <rory.christian.murray@gmail.com>
2019-09-16 08:37:28 -0400

    Create POST new user endpoint

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ const server = express();
 
 // tell express to parse JSON
 server.use(express.json());
+
+// POST a new user
+server.post("/api/users", (req, res) => {
+  console.log("newUser body", newUser);
+  const newUser = req.body;
+
+  if (!newUser.name || !newUser.bio) {
+    res.status(400).json({ message: "Name and bio required" });
+  } else {
+    db.insert(newUser)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(error => {
+        res.status(500).json({ message: "Error adding new user" });
+      });
+  }
+});
+
+const port = 8000;
+server.listen(port, () => {
+  console.log(`API running on port ${port} `);
+});
